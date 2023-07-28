0fc7539e4fda0c07989ba7c4b63d24988dad9e75
Salvador Rodriguez <chava.crb@gmail.com>
2023-07-28 12:25:53 -0600

    Add DOM elements

---
+++ src/main.js
@@ -0,0 +1,7 @@
+// DOM ELEMENTS
+let submitBtn = document.getElementById('add-btn');
+let cleanBtn = document.getElementById('clean-btn');
+let logOutBtn = document.getElementById('logOut-btn');
+let nameInput = document.getElementById('logOut-btn');
+let lastNameInput = document.getElementById('logOut-btn');
+let ageInput = document.getElementById('logOut-btn');
